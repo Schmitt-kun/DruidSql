2fae369db79c3cc7b4e1d5100c846c74e9491d78
Schmitt <ingvarrschmitt@gmail.com>
2018-03-22 21:43:01 +0300

    Main class

---
+++ src/ru/kuznetsov/sqldispatch/Main.java
@@ -0,0 +1,11 @@
+package ru.kuznetsov.sqldispatch;
+
+public class Main {
+
+	public static void main(String[] args) {
+		if (args.length == 0) {
+			System.out.println("Usage SqlDispatch [SqlQuery]");
+			return;
+		}
+	}
+}
